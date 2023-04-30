dc527a5482ed78bfa1825ad56aeef0966a9d0f04
Vachik_Tovmasyan <tovm2504@mail.ru>
2023-05-01 00:34:50 +0300

    5 Семинар. ДЗ

--- 3. Специализация/1. Python/3 Лекция + 5-6 Семинар/3 Лекция 2 Рекурсия Фибаначии + Сортировка.py
+++ 3. Специализация/1. Python/3 Лекция + 5-6 Семинар/3 Лекция 2 Рекурсия Фибаначии + Сортировка.py
@@ -1,4 +1,4 @@
-# 1
+# 1                                 Рекурсия. Числа Фибаначи
 # def fib(n):
 #     if n in [1, 2]: return 1
 #     else: return fib(n-1) + fib(n-2)
@@ -25,10 +25,19 @@
 # print(printFib)
 
 
+# 4
+# n = int(input("Введите число Факториала: "))
+# def fib(n):
+#     if n in [1, 2]: return 1
+#     else: 
+#         return fib(n-1) + fib(n-2)
+# list1 = [fib(i) for i in range(1,10)]
+# print(list1)
 
 
 
 
+#                              Быстрая сортировка
 # def quick_sort(array):
 #     if len(array) <= 1:
 #         return array
@@ -60,3 +69,25 @@
 #     return sortik(lesss1) + [pointik] + sortik(lesss2)
 # print(sortik([4, 49, 44, 16, 25, 25, 19]))
 
+
+import random
+
+n = int(input("Введите размер массива: "))
+array1 = list()
+for i in range(0,n):
+    array1.append(random.randint(1,50))
+
+
+
+def sort(array):
+    if len(array) == 1: return 1
+    else:
+        count = [0]
+        left = [i for i in array[1:] if i <= count]
+        rigth = [i for i in array[:1] if i > count]    
+    return sort(left) + [count] + sort(rigth)
+    
+
+
+print(array1)
+print(sort([array]))
