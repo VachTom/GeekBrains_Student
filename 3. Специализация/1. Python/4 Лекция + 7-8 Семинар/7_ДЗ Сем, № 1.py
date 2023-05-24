7b1fd4b5458ee2b8ac8bf865baf26c4461638ae0
Vachik_Tovmasyan <tovm2504@mail.ru>
2023-05-24 19:07:13 +0300

    7 Семинар. ДЗ

--- 3. Специализация/1. Python/4 Лекция + 7-8 Семинар/7_ДЗ Сем, № 1.py
+++ 3. Специализация/1. Python/4 Лекция + 7-8 Семинар/7_ДЗ Сем, № 1.py
@@ -14,6 +14,22 @@
     **Вывод:** Парам пам-пам  
  """
 
-vini = input("Фраза Винни-Пуха: ")
-print(vini)
+
+def Vini(str):
+    str = str.split()
+    list = []
+    for word in str:
+        result = 0
+        for i in word:
+            if i in 'аеёиоуыэюя':
+                result += 1
+        list.append(result)
+    return len(list) == list.count(list[0])
+
+str = input('Введите стихотворение Винни-пуха: ')
+if Vini(str):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
+
 
